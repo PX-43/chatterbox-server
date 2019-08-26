4ea80617df38205738c20ddc63781e14836b5119
px-43 <z.konyves@gmail.com>
2019-08-26 21:43:25 +0100

    init commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,44 @@
+import WebSocket from 'ws';
 
+interface Incoming {
+    id: string;
+    message: string;
+    name: string;
+}
 
+let wsServeStarted = false;
+const sessions = new Map();
+function start() {
 
+    if (wsServeStarted) {
+        console.error('ws server already started');
+        return;
+    }
+    const wss = new WebSocket.Server({ port: 8080 });
+
+    wss.on('connection', ws => {
+        ws.on('message', (incoming: string) => {
+            const cleanIncoming: Incoming = JSON.parse(incoming);
+            if (!sessions.has(cleanIncoming.id)) {
+                sessions.set(cleanIncoming.id, ws);
+            }
+
+            Array.from( sessions.keys())
+                .filter((k: string) => k !== cleanIncoming.id)
+                .forEach((s: string) => {
+                    if (cleanIncoming.message !== undefined && cleanIncoming.message.length > 0) {
+                        sessions.get(s)
+                            .send(JSON.stringify({message: cleanIncoming.message, name: cleanIncoming.name}));
+                    }
+                });
+        });
+
+        // ws.send('CONNECTED');
+    });
+
+    console.log('listening on http://localhost:8080');
+
+    wsServeStarted = true;
+}
+
+start();
